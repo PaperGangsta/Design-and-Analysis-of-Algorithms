c0e21e625c61e73508fb8d4fc9bde0e861561057
YanQiMing <1024744235@qq.com>
2020-03-23 21:19:43 +0800

    完成二分归并

---
+++ 作业4/二分归并排序/二分归并排序/二分归并排序.cpp
@@ -0,0 +1,49 @@
+﻿#include<algorithm>
+#include<cstdio>
+#include<cmath>
+#include<iostream>
+using namespace std;
+
+const int n = 10;
+const int maxn = 100;
+
+void merge(int a[], int l1, int r1, int l2, int r2) {
+	int i = l1, j = l2;
+	int temp[maxn], index = 0;
+	while (i <= r1 && j <=r2) {
+		if (a[i] <= a[j]) {
+			temp[index++] = a[i++];
+		}
+		else {
+			temp[index++] = a[j++];
+		}
+	}
+	while (i<=r1){
+		temp[index++] = a[i++];
+	}
+	while (j<=r2){
+		temp[index++] = a[j++];
+	}
+	for (i = 0; i < index; i++) {
+		a[l1 + i] = temp[i];
+	}
+}
+
+void mergeSort(int a[]) {
+	for (int step = 2; step / 2 <= n; step *= 2) {
+		for (int i = 1; i <= n; i += step) {
+			int mid = i + step / 2 - 1;
+			if (mid + 1 <= n){
+				merge(a, i, mid, mid + 1, min(i + step - 1, n));
+			}
+		}
+	}
+}
+
+int main() {
+	int a[n] = { 1,2,3,4,5,6,7,8,9,10};
+	//mergeSort(a);
+	for (int i = 0; i < n; i++) {
+		printf("%d ", a[i]);
+	}
+}
